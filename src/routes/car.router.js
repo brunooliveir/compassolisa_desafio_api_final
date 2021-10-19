8735a78c3ea93f68b778dda61238b945a33da3da
brunooliveir <brunooliveir199@gmail.com>
2021-10-19 10:56:44 -0300

    fix(car.router): route now has prefix '/api/v1/car'

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,7 +1,7 @@
 const CarController = require('../app/controller/CarController')
 const createValidation = require('../app/validation/car/create')
 
-module.exports = (server, routes, prefix = '/car') => {
+module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/', createValidation, CarController.create)
     server.use(prefix, routes)
 }
