6b621c1b5fb98f3b7ce70e738c6c87962d460a7f
brunooliveir <brunooliveir199@gmail.com>
2021-11-02 15:39:19 -0300

    feat(swagger): add swagger in .../api-docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express')
 const car = require('../routes/car.router')
 const people = require('../routes/people.router')
 const authenticate = require('../routes/authenticate.router')
+const apidocs = require('../routes/api-docs.router')
 
 module.exports = server => {
     server.use((req, res, next) => {
+        apidocs(server, new Router())
         car(server, new Router())
         people(server, new Router())
         authenticate(server, new Router())
