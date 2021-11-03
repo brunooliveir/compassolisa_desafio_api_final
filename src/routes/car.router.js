42dcecb88f1d7b77e5b9df3d4a73a729b573f12a
brunooliveir <brunooliveir199@gmail.com>
2021-11-02 22:50:08 -0300

    feat(car route patch): added route patch in car

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,6 +1,7 @@
 const CarController = require('../app/controller/CarController')
 const createValidation = require('../app/validation/car/create')
 const idValidation = require('../app/validation/car/id')
+const acessoriosValidation = require('../app/validation/car/acessorios')
 const errors = require('../app/errors/car/index')
 
 
@@ -9,6 +10,7 @@ module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.get('/', CarController.listQuery, errors)
     routes.get('/:id', idValidation, errors, CarController.findOneById, errors)
     routes.put('/:id', idValidation, errors, createValidation, errors, CarController.updateById, errors)
+    routes.patch('/:id/acessorios/:id_acessorio', acessoriosValidation, CarController.updateAcessorioById, errors)
     routes.delete('/:id', idValidation, errors, CarController.deleteOne, errors)
     server.use(prefix, routes)
 }
